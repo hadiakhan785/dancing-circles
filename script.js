b09dfd49208b836a2c511a7df03c0ed3a3ca9415
hadiakhan785 <hadiakhan785@gmail.com>
2018-07-28 13:53:11 +0500

    first

---
+++ script.js
@@ -0,0 +1,29 @@
+function initialize() {
+    const bodyEl = document.querySelector("body");
+    for(let i=0; i<= Math.floor(Math.random() * 12); i++) {
+        bodyEl.innerHTML += `<div id="div-${i}"></div>`;
+        setInterval(() => {changeCircleSize(i)},1000);
+    }
+}
+
+
+function changeCircleSize(i) {
+    let x = 1;
+    let circle = document.querySelector(`#div-${i}`);
+    let position = parseInt(Math.random()*500);
+    let size = parseInt(Math.random()*300);
+    let red = parseInt(Math.random()* 256);
+    let green = parseInt(Math.random()* 256);
+    let blue = parseInt(Math.random()* 256);
+
+    circle.style.height = size + "px";
+    circle.style.width = size + "px";
+    circle.style.color = "rgb(" + red + ", " + green + ", " + blue + ")" ;
+    circle.style["border-color"] = "rgb(" + red + ", " + green + ", " + blue + ")";
+    circle.style.border = "3px solid rgb(" + red + ", " + green + ", " + blue + ")";
+    circle.style["font-size"] = size / 50 +"em";
+    circle.style["margin-top"] = `${Math.floor(Math.random() * 5)}vh`;
+    circle.style["margin-right"] = `${Math.floor(Math.random() * 5)}vw`;
+    circle.innerHTML = parseInt(Math.random()*300);
+
+}
